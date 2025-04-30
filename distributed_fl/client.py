5e376765c87e08362e95409a4a3710ca2fbcb8f0
tlebryk <tlebryk02@gmail.com>
2025-04-29 20:00:53 -0400

    push changes

--- distributed_fl/client.py
+++ distributed_fl/client.py
@@ -236,9 +236,7 @@ class FederatedClient:
         # train agent.model here
         # get training data
         # TODO: figure out file paths
-        train_dataset = create_huggingface_dataset(
-            "/home/tlebryk/262_distributed_systems/Distributed-FL/data"
-        )
+        train_dataset = create_huggingface_dataset("./Distributed-FL/data")
         print(f"{len(train_dataset)=}")
         # train agent.model
         personal_adapters = os.path.join(PATH_TO_ADAPTERS, "client", "personal")
